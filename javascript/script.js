97938c4f42e6647bb3532229ef966214b40a484a
Topmark1 <topsiology11@gmail.com>
2022-11-24 21:08:09 +0100

    modern website prototype

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+/* typing Animation */
+var typed = new Typed('.typing',{
+    strings:['','Blockchain Develope','Web Developer','Smart Contract Expert','Digital Expert'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
